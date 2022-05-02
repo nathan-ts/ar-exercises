16056219f1b8a50246154960db310a138ec349b0
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-05-02 23:38:45 +0000

    add exercise 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,7 +3,6 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
 
 class Stores < ActiveRecord::Base
 
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Stores.find(1)
+@store2 = Stores.find(2)
+# puts @store1.name
+# puts @store2.name
+@store1.name = 'Toronto'
+# puts @store1.name
